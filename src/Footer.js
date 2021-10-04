d7018c1a05c3a5d70f908299fd11bfc95388c054
Gualdo11 <gualdo11@gmail.com>
2021-10-04 16:34:41 -0300

    Cambios Footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,19 +1,20 @@
 import { faBell, faMailBulk } from '@fortawesome/free-solid-svg-icons'
 import React, { Component } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faGithub, faInstagram, faLinkedin, faReact, faTwitter } from '@fortawesome/free-brands-svg-icons'
+import { faGithub, faInstagram, faLinkedin, faReact, faTwitter, faDrupal } from '@fortawesome/free-brands-svg-icons'
 
 export default class Footer extends Component {
   render() {
     return (
       <div className="footer">
         <div className="iconos">
-          <FontAwesomeIcon icon={faMailBulk} />
-          <FontAwesomeIcon icon={faGithub} />
-          <FontAwesomeIcon icon={faReact} />
-          <FontAwesomeIcon icon={faLinkedin} />
-          <FontAwesomeIcon icon={faTwitter} />
-          <FontAwesomeIcon icon={faInstagram} />
+          <a href="" className="l-f"><FontAwesomeIcon icon={faMailBulk} /></a>
+          <a href="https://github.com/Gualdo11/" target="_blank" className="l-f"><FontAwesomeIcon icon={faGithub} /></a>
+          <a href="" className="l-f"><FontAwesomeIcon icon={faReact} /></a>
+          <a href="" className="l-f"><FontAwesomeIcon icon={faDrupal} /></a>    
+          <a href="https://www.linkedin.com/in/juan-ignacio-guallini-3743b450/" target="_blank" className="l-f"> <FontAwesomeIcon icon={faLinkedin} /></a>
+          <a href="https://twitter.com/Gualdo11" target="_blank" className="l-f"><FontAwesomeIcon icon={faTwitter} /></a>
+          <a href="http://instagram.com/Gualdo11" target="_blank" className="l-f"><FontAwesomeIcon icon={faInstagram} /></a>    
         </div>
       </div>
     )
